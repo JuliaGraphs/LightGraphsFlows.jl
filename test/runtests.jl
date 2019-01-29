78c9cb367e760ceb0a7c1352b7f8b34c38a7e8fb
Nicholas Dinsmore <nicholas.dinsmore@gmail.com>
2019-01-29 12:42:49 -0500

    Fix missing test

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,12 +12,12 @@ testgraphs(g) = [g, lg.Graph{UInt8}(g), lg.Graph{Int16}(g)]
 testdigraphs(g) = [g, lg.DiGraph{UInt8}(g), lg.DiGraph{Int16}(g)]
 
 for t in [
-        # "edmonds_karp",
-        # "dinic",
-        # "boykov_kolmogorov",
-        # "push_relabel",
-        # "maximum_flow",
-        # "multiroute_flow",
+        "edmonds_karp",
+        "dinic",
+        "boykov_kolmogorov",
+        "push_relabel",
+        "maximum_flow",
+        "multiroute_flow",
         "mincost",
         "mincost_dep",  #Please remove this once depreciated mincost_flow iterface is removed
         "mincut",
